f9bc11745e63e7d85c35f1acfebe1bebd20e72a8
Mateus <mateuspaivamatiazzi1@gmail.com>
2020-06-19 18:02:08 -0300

    primeiro teste de consumir api

--- mobile/app/src/main/java/com/example/mobile/Livro.kt
+++ mobile/app/src/main/java/com/example/mobile/Livro.kt
@@ -1,4 +1,3 @@
 package com.example.mobile
 
-data class Livro (val livroId:Int, val ) {
-}
+data class Livro (val livroId:Int, val autorId:Int, val titulo:String, val dataDePublicacao:String, val nomeDoAutor:String)
--- mobile/app/src/main/java/com/example/mobile/MainActivity.kt
+++ mobile/app/src/main/java/com/example/mobile/MainActivity.kt
@@ -1,18 +1,65 @@
 package com.example.mobile
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var resultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultado = findViewById(R.id.resultado)
+        val url = "http://localhost/desafio-mobile/api/livros/retornaLivros.php"
+
+        AsyncTaskHandleJson().execute(url)
+    }
+
+    inner class AsyncTaskHandleJson : AsyncTask<String, String, String>(){
+        override fun doInBackground(vararg url: String?): String {
+            var text: String
+            val connection = URL(url[0]).openConnection() as HttpURLConnection
+            try{
+                connection.connect()
+                text = connection.inputStream.use{ it.reader().use{reader -> reader.readText()} }
+            }finally {
+                connection.disconnect()
+            }
+            return text
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            handleJson(result)
+        }
+
+    }
+
+    private fun handleJson(jsonString : String?) {
+        val jsonArray = JSONArray(jsonString)
+
+        val list = ArrayList<Livro>()
+
+        var x = 0
+
+        while(x < jsonArray.length()){
+            val jsonObject = jsonArray.getJSONObject(x)
+
+            list.add(Livro(
+                jsonObject.getInt("livroId"),
+                jsonObject.getInt("autorId"),
+                jsonObject.getString("titulo"),
+                jsonObject.getString("dataDePublicacao"),
+                jsonObject.getString("nomeDoAutor")
+            ))
+            x++
+        }
+        val adapter = AdaptadorDeLista(this, list)
+        livros_list.adapter = adapter
     }
 
 }

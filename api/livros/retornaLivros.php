bafbb438707bbe3ff3309f999b94a0c4d890db78
Mateus <mateuspaivamatiazzi1@gmail.com>
2020-06-19 11:20:23 -0300

    Adicao da condicao de nao existencia de nenhum livro

--- api/livros/retornaLivros.php
+++ api/livros/retornaLivros.php
@@ -33,5 +33,12 @@ if($quantidadeDeLivrosNoBancoDeDados > 0){
     http_response_code(200);
 
     echo json_encode($livrosArray);
+}else{
+  
+    http_response_code(404);
+  
+    echo json_encode(
+        array("message" => "Nenhum livro encontrado.")
+    );
 }
 ?>

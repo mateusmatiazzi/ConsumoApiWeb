21472d7ca05f152ed9be5fe4fd354c77caad9630
Mateus <mateuspaivamatiazzi1@gmail.com>
2020-06-19 12:57:06 -0300

    mudança na forma em que os livros são exibidos

--- api/objetos/livro.php
+++ api/objetos/livro.php
@@ -16,7 +16,7 @@ class Livro{
     public function read(){
         $query = "SELECT a.nome as nomeDoAutor, l.livroId, l.titulo, l.dataDePublicacao, l.autorId
                   FROM " . $this->nomeDaTabela . " l LEFT JOIN autor a ON l.autorId = a.autorId
-                  ORDER BY l.dataDePublicacao DESC";
+                  ORDER BY l.livroId ASC";
 
         $ordem = $this->conexaoComBancoDeDados->prepare($query);
         $ordem->execute();
